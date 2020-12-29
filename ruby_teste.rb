73c4f92e7c108dc31327ae158ca1f83cfeb59d43
Nathan Guilherme Gomes <nathan.ggomes@gmail.com>
2020-12-29 13:51:57 -0300

    teste

---
+++ ruby_teste.rb
@@ -0,0 +1,40 @@
+
+num=gets.to_i
+
+
+if (num) % 3 == 0 && (num) % 5 == 0
+    puts "baz"
+elsif (num) % 3 == 0
+    puts "foo"
+elsif (num) % 5 == 0
+    puts "bar"
+else
+    puts (num)
+end
+
+
+
+
+
+
+
+
+
+
+
+# 1
+# 2
+# 3 / 3
+# 4  
+# 5 / 5
+# 6 /3
+# 7
+# 8
+# 9 / 3
+# 10 / 5
+# 11
+# 12 / 3
+# 13 
+# 14
+# 15 / 3  / 5
+# 16 /
